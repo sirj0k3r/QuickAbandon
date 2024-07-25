e93f4502b467639a186aef6034de81477c213bd5
sirj0k3r <info.ppf@sapo.pt>
2024-07-25 02:33:59 +0100

    + INITIAL COMMIT

---
+++ QuickAbandon.lua
@@ -0,0 +1,44 @@
+questID = nil;
+
+function QuickAbandonQuest()
+	if (CheckKeybind() and QuestChecks()) then
+		C_QuestLog.SetSelectedQuest(questID); 
+		C_QuestLog.SetAbandonQuest(); 
+		C_QuestLog.AbandonQuest();
+
+		WorldMapFrameCloseButton:Click();
+		
+		print("|cFFFFF00AQuest abandoned: ".. GetQuestLink(questID) .."");
+	end
+end
+
+hooksecurefunc("QuestMapLogTitleButton_OnClick", function(self, button)
+	SetQuestID(self.questID);
+	QuickAbandonQuest();
+end)
+
+hooksecurefunc(CampaignQuestObjectiveTracker, "OnBlockHeaderClick", function(self, block, button)
+	SetQuestID(block.id);
+	QuickAbandonQuest();
+end)
+
+hooksecurefunc(QuestObjectiveTracker, "OnBlockHeaderClick", function(self, block, button)
+	SetQuestID(block.id);
+	QuickAbandonQuest();
+end)
+
+-- Checks if the keybind is present
+function CheckKeybind()
+	result = IsAltKeyDown();
+	return result;
+end
+
+-- Checks if a quest can be abandoned
+function QuestChecks()
+	result = C_QuestLog.CanAbandonQuest(questID) and C_QuestLog.IsOnQuest(questID)
+	return result;
+end
+
+function SetQuestID(id)
+	questID = id;
+end
